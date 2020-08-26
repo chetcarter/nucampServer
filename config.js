aaa8fc39041c437166bf959a730d03f31dc223a4
Chet Carter <abazabalv@gmail.com>
2020-08-25 19:01:59 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	secretKey: '12345-67890-09876-54321',
 	mongoUrl: 'mongodb://localhost:27017/nucampsite',
+	facebook: {
+		clientId: '341247213901522',
+		clientSecret: 'd22b3d1469a17e72dad8a94a659e46e1',
+	},
 };
